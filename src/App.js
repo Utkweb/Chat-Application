053b47da3731276afda43595267aca35d33f7127
Utkweb <us196000@gmail.com>
2023-05-18 08:04:45 +0530

    just done with registration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Content />} path="/content" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Register />} path="/register" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="#">
+            {/* <img
+              src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+              alt="Logo"
+              width="30"
+              height="24"
+              className="d-inline-block align-text-top"
+            /> */}
+            Bootstrap
+          </NavLink>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
